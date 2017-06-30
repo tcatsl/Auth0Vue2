f8862a5c07c6a7051e8111b6f9561bac6a5828ba
Thomas Castleman <thomas.castleman@gmail.com>
2017-06-29 23:31:44 -0600

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-// import Hello1 from '@/components/Hello1'
 import Auth from '@/components/auth'
 import callback from '@/components/callback'
-// import Main from '@/components/Main'
 Vue.use(Router)
 
 export default new Router({
@@ -14,8 +12,7 @@ export default new Router({
       name: 'Hello',
       component: Hello,
     },
-    { path: '/callback', component: callback},
-    { path: '/game', component: Hello}
+    { path: '/callback', component: callback}
   ],
 
   mode: 'history'
