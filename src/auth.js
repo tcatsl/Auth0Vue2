2ec11cddb2c622ce6e2ffeeae547c1d3d65d1429
Thomas Castleman <thomas.castleman@gmail.com>
2017-06-29 23:47:54 -0600

    add updates, comments

--- src/auth.js
+++ src/auth.js
@@ -1,34 +1,24 @@
-import decode from 'jwt-decode';
-import auth0 from 'auth0-js';
-import Router from 'vue-router';
-import Auth0Lock from 'auth0-lock';
-const ID_TOKEN_KEY = 'id_token';
-const ACCESS_TOKEN_KEY = 'access_token';
-const CLIENT_ID = '';
-const CLIENT_DOMAIN = '';
-const REDIRECT = 'http://localhost:8080/callback';
-
 var auth = new auth0.WebAuth({
   clientID: CLIENT_ID,
   domain: CLIENT_DOMAIN
-});
+})
 
 export function login() {
   auth.authorize({
     responseType: 'token id_token',
     redirectUri: REDIRECT,
     scope: "openid email"
-  });
+  })
 }
 
 var router = new Router({
    mode: 'history',
-});
+})
 
 export function logout() {
-  clearIdToken();
-  clearAccessToken();
-  router.go('/');
+  clearIdToken()
+  clearAccessToken()
+  router.go('/')
 }
 
 export function requireAuth(to, from, next) {
@@ -36,62 +26,74 @@ export function requireAuth(to, from, next) {
     next({
       path: '/',
       query: { redirect: to.fullPath }
-    });
+    })
   } else {
-    next();
+    next()
   }
 }
 
 export function getIdToken() {
-  return localStorage.getItem(ID_TOKEN_KEY);
+  return localStorage.getItem(ID_TOKEN_KEY)
 }
 
 export function getAccessToken() {
-  return localStorage.getItem(ACCESS_TOKEN_KEY);
+  return localStorage.getItem(ACCESS_TOKEN_KEY)
 }
 
 function clearIdToken() {
-  localStorage.removeItem(ID_TOKEN_KEY);
+  localStorage.removeItem(ID_TOKEN_KEY)
 }
 
 function clearAccessToken() {
-  localStorage.removeItem(ACCESS_TOKEN_KEY);
+  localStorage.removeItem(ACCESS_TOKEN_KEY)
 }
 
 // Helper function that will allow us to extract the access_token and id_token
 function getParameterByName(name) {
-  let match = RegExp('[#&]' + name + '=([^&]*)').exec(window.location.hash);
-  return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+  var match = RegExp('[#&]' + name + '=([^&]*)').exec(window.location.hash)
+  return match && decodeURIComponent(match[1].replace(/\+/g, ' '))
 }
 
 // Get and store access_token in local storage
 export function setAccessToken() {
-  let accessToken = getParameterByName('access_token');
-  localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+  var accessToken = getParameterByName('access_token')
+  localStorage.setItem(ACCESS_TOKEN_KEY, accessToken)
 }
 
 // Get and store id_token in local storage
 export function setIdToken() {
-  let idToken = getParameterByName('id_token');
-  localStorage.setItem(ID_TOKEN_KEY, idToken);
+  var idToken = getParameterByName('id_token')
+  localStorage.setItem(ID_TOKEN_KEY, idToken)
 }
-
+//before returning, clear the token if expired
 export function isLoggedIn() {
-  const idToken = getIdToken();
-  return !!idToken && !isTokenExpired(idToken);
+  const idToken = getIdToken()
+  if (!!idToken && !!isTokenExpired(idToken)){
+    clearIdToken()
+    clearAccessToken()
+  }
+  return !!idToken && !isTokenExpired(idToken)
 }
 
 function getTokenExpirationDate(encodedToken) {
-  const token = decode(encodedToken);
-  if (!token.exp) { return null; }
+  const token = decode(encodedToken)
+  if (!token.exp) { return null }
 
-  const date = new Date(0);
-  date.setUTCSeconds(token.exp);
+  const date = new Date(0)
+  date.setUTCSeconds(token.exp)
+  return date
+}
 
-  return date;
+//a useful function to return the user's email address
+export function returnEmail() {
+  if (!!isLoggedIn()){
+    const token = decode(getIdToken())
+    return token.email
+  }
+  return null
 }
 
 function isTokenExpired(token) {
-  const expirationDate = getTokenExpirationDate(token);
-  return expirationDate < new Date();
+  const expirationDate = getTokenExpirationDate(token)
+  return expirationDate < new Date()
 }
